a25e6eec75065f99511a2309150a13b63e404552
James Abley <james.abley@gmail.com>
2017-04-26 16:47:11 +0100

    WIP
    adr-config loses the executable bit for some reason.

--- adr_tools.rb
+++ adr_tools.rb
@@ -1,37 +1,17 @@
 class AdrTools < Formula
   desc "A command-line tool for working with Architecture Decision Records (ADRs)."
   homepage "https://github.com/npryce/adr-tools"
-  url "https://codeload.github.com/npryce/adr-tools/tar.gz/1.2.0"
-  version "1.2.0"
-  sha256 "3f45646c099ae996b97c855a9a27ed540e076580eba59c2e8613453b7a67e412"
+  url "https://codeload.github.com/npryce/adr-tools/tar.gz/2.1.0"
+  version "2.1.0"
+  sha256 "1ef028cfeaa1b262a5c62845aa8965be169705370983f9ff73b17ec77bf75f70"
 
   def install
-    inreplace 'src/adr-init' do |filename|
-      filename.gsub! '$(dirname $0)', prefix
-      filename.gsub! "#{prefix}/config.sh", "#{prefix}/bin/config.sh"
-      filename.gsub! "$adr_template_dir/init.md", "#{prefix}/init.md"
-    end
-
-    %w{
-      _adr_commands 
-      _adr_dir 
-      _adr_file 
-      _adr_generate_toc 
-      _adr_title 
-      _adr_update_status
-      adr
-      adr-generate 
-      adr-help
-      adr-list 
-      adr-new
-    }.each do |filename|
-      inreplace "src/#{filename}", '$(dirname $0)/config.sh', "#{prefix}/bin/config.sh"
-    end 
-    
+    inreplace 'src/adr-init', "$adr_template_dir/init.md", "#{prefix}/init.md"
     inreplace 'src/adr-new', '$adr_template_dir/template.md', "#{prefix}/template.md"
 
     prefix.install Dir["src/*.md"]
     bin.install Dir["src/*"] - Dir["src/*.md"]
+    chmod 0555, "#{bin}/adr-config"
   end
 
   test do
